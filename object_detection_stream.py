43021d967712807f308ddf7b35221fe41ea6ce25
stormrise <lilingxiao0414@gmail.com>
2019-06-30 11:19:15 +0800

    format

--- object_detection_stream.py
+++ object_detection_stream.py
@@ -25,8 +25,6 @@ output_topic = 'output'
 brokers = "G01-01:2181,G01-02:2181,G01-03:2181,G01-04:2181,G01-05:2181,G01-06:2181,G01-07:2181,G01-08:2181," \
           "G01-09:2181,G01-10:2181,G01-11:2181,G01-12:2181,G01-13:2181,G01-14:2181,G01-15:2181,G01-16:2181"
 
-
-
 kafkaStream = KafkaUtils.createStream(ssc, brokers, 'test-consumer-group', {input_topic: 10},
                                       valueDecoder=my_decoder)
 producer = KafkaProducer(bootstrap_servers='G01-01:9092', compression_type='gzip', batch_size=163840,
@@ -37,7 +35,9 @@ detector.setModelTypeAsRetinaNet()
 detector.setModelPath('/home/hduser/objectDetection/resnet50_coco_best_v2.0.1.h5')
 detector.loadModel()
 graph = tf.get_default_graph()
-#detector.loadModel(detection_speed="fastest")
+
+
+# detector.loadModel(detection_speed="fastest")
 
 
 def handler(message):
@@ -58,18 +58,21 @@ def handler(message):
         global graph
         global model
         with graph.as_default():
-                image = np.asarray(bytearray(value), dtype="uint8")
-                # image = np.frombuffer(value, dtype=np.uint8)
-                # img = image.reshape(300, 400, 3)
-                # img = cv2.imread("/tmp/" + key)
-                img = cv2.imdecode(image, cv2.IMREAD_ANYCOLOR)
-                img_array = np.array(img)
-                detected_image_array, detections = detector.detectObjectsFromImage(input_type="array", input_image=img_array ,output_type="array")
-                #image_really = Image.fromarray(detected_image_array.astype('uint8')).convert('RGB')
+            image = np.asarray(bytearray(value), dtype="uint8")
+            # image = np.frombuffer(value, dtype=np.uint8)
+            # img = image.reshape(300, 400, 3)
+            # img = cv2.imread("/tmp/" + key)
+            img = cv2.imdecode(image, cv2.IMREAD_ANYCOLOR)
+            img_array = np.array(img)
+            detected_image_array, detections = detector.detectObjectsFromImage(input_type="array",
+                                                                               input_image=img_array,
+                                                                               output_type="array")
+            # image_really = Image.fromarray(detected_image_array.astype('uint8')).convert('RGB')
 
-                producer.send(output_topic, value=cv2.imencode('.jpg', detected_image_array)[1].tobytes(), key=key.encode('utf-8'))
-                producer.flush()
-                print('send over!')
+            producer.send(output_topic, value=cv2.imencode('.jpg', detected_image_array)[1].tobytes(),
+                          key=key.encode('utf-8'))
+            producer.flush()
+            print('send over!')
 
 
 kafkaStream.foreachRDD(handler)
