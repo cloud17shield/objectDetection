c01cebdb8f0bcdd9220d9b79bcaf9ecd1bfe8274
Ranxin <565640524@qq.com>
2019-07-05 17:12:01 +0800

    partition 15

--- object_detection_stream.py
+++ object_detection_stream.py
@@ -30,7 +30,7 @@ def my_decoder(s):
     return s
 
 
-kafkaStream = KafkaUtils.createStream(ssc, brokers, 'test-consumer-group', {input_topic: 10},
+kafkaStream = KafkaUtils.createStream(ssc, brokers, 'test-consumer-group', {input_topic: 15},
                                       valueDecoder=my_decoder)
 producer = KafkaProducer(bootstrap_servers='G01-01:9092', compression_type='gzip', batch_size=163840,
                          buffer_memory=33554432, max_request_size=20485760)
