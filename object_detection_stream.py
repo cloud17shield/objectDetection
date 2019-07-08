17e6152a98d3aef5d609e597dbcd333b32fa3b43
stormrise <lilingxiao0414@gmail.com>
2019-07-08 15:03:59 +0800

    output3

--- object_detection_stream.py
+++ object_detection_stream.py
@@ -24,7 +24,7 @@ sc.setLocalProperty("spark.scheduler.pool", "pool3")
 ssc = StreamingContext(sc, 0.5)
 sql_sc = SQLContext(sc)
 input_topic = 'input'
-output_topic = 'output'
+output_topic = 'output3'
 brokers = "G01-01:2181,G01-02:2181,G01-03:2181,G01-04:2181,G01-05:2181,G01-06:2181,G01-07:2181,G01-08:2181," \
           "G01-09:2181,G01-10:2181,G01-11:2181,G01-12:2181,G01-13:2181,G01-14:2181,G01-15:2181,G01-16:2181"
 
